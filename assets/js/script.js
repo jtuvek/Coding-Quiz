c3a2aee54d7cd424c3c0f4831f338b7284c0d77f
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-06-26 21:27:20 -0400

    fixed questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,130 @@
 // Quiz Questions
 var questions = [
     {
-      question: "Question 1: What does HTML stand for?",
-      choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "None of the above"],
-      correctAnswer: "Hyper Text Markup Language"
+      question: "What is HTML?",
+      choices: [
+        "Hyper Text Markup Language",
+        "Hyperlinks and Text Markup Language",
+        "Home Tool Markup Language",
+        "None of the above"
+      ],
+      answer: "Hyper Text Markup Language"
     },
     {
-      question: "Question 2: Which symbol is used to access the ID in CSS?",
-      choices: ["#", ".", "$", "@"],
-      correctAnswer: "#"
+      question: "What is CSS?",
+      choices: [
+        "Cascading Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "None of the above"
+      ],
+      answer: "Cascading Style Sheet"
     },
     {
-      question: "Question 3: What is the correct syntax to create a function in JavaScript?",
-      choices: ["function myFunction()", "function:myFunction()", "function = myFunction()", "myFunction() = function"],
-      correctAnswer: "function myFunction()"
+      question: "What is JavaScript?",
+      choices: [
+        "A programming language",
+        "A scripting language",
+        "Both of the above",
+        "None of the above"
+      ],
+      answer: "Both of the above"
     },
     {
-      question: "Question 4: Which programming language is used for styling web pages?",
-      choices: ["HTML", "CSS", "JavaScript", "Python"],
-      correctAnswer: "CSS"
+      question: "What is a variable in JavaScript?",
+      choices: [
+        "A container to store data",
+        "A function that performs a specific task",
+        "A way to define HTML tags",
+        "None of the above"
+      ],
+      answer: "A container to store data"
     },
     {
-      question: "Question 5: What is the output of the following code?\nvar x = 5;\nconsole.log(x === '5');",
-      choices: ["true", "false", "TypeError", "ReferenceError"],
-      correctAnswer: "false"
+      question: "What does the 'console.log()' function do?",
+      choices: [
+        "Prints output to the console",
+        "Changes the background color of a web page",
+        "Creates a new HTML element",
+        "None of the above"
+      ],
+      answer: "Prints output to the console"
     },
     {
-      question: "Question 6: Which operator is used to concatenate strings in JavaScript?",
-      choices: ["+", "-", "*", "/"],
-      correctAnswer: "+"
+      question: "What is the purpose of a 'for' loop in JavaScript?",
+      choices: [
+        "To repeat a block of code a specific number of times",
+        "To define a function",
+        "To select elements from the DOM",
+        "None of the above"
+      ],
+      answer: "To repeat a block of code a specific number of times"
     },
     {
-      question: "Question 7: What is the purpose of the 'this' keyword in JavaScript?",
-      choices: ["To create a new object", "To refer to the current object", "To import external libraries", "To loop over an array"],
-      correctAnswer: "To refer to the current object"
+      question: "What is the CSS box model used for?",
+      choices: [
+        "Describing the layout and spacing of elements on a web page",
+        "Defining colors and backgrounds",
+        "Handling user interactions and events",
+        "None of the above"
+      ],
+      answer: "Describing the layout and spacing of elements on a web page"
     },
     {
-      question: "Question 8: What does CSS stand for?",
-      choices: ["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
-      correctAnswer: "Cascading Style Sheets"
-    },
+      question: "What is the purpose of the 'addEventListener()' method in JavaScript?",
+      choices: [
+        "To attach an event handler function to an element",
+        "To create a new HTML element",
+        "To perform mathematical calculations",
+        "None of the above"
+      ],
+      answer: "To attach an event handler function to an element"
+    }
   ];
   
-  // Other variables
-var currentQuestionIndex = 0;
-var timeLeft = 60;
-var timerInterval;
-var score = 0;
-var initialsInput = document.getElementById("initials");
-var highScoresList = document.getElementById("high-scores-list");
-var startContainer = document.getElementById("start-container");
+ // DOM Elements
+var startButton = document.getElementById("start-btn");
 var questionContainer = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
 var choicesElement = document.getElementById("choices");
-var feedbackElement = document.getElementById("feedback");
+var messageElement = document.getElementById("message");
+var timerElement = document.getElementById("time-left");
+var gameOverContainer = document.getElementById("game-over-container");
+var finalScoreElement = document.getElementById("final-score");
+var scoreForm = document.getElementById("score-form");
+var initialsInput = document.getElementById("initials");
+var highScoresList = document.getElementById("high-scores-list");
 
-// Start the quiz
+var currentQuestionIndex;
+var timerInterval;
+var timeLeft;
+var highScores = [];
+
 function startQuiz() {
-  startContainer.style.display = "none";
-  questionContainer.style.display = "block";
+  currentQuestionIndex = 0;
+  timeLeft = 60;
+  startTimer();
   showQuestion();
+  hideElement(startButton);
+  showElement(questionContainer);
 }
 
-// Show a question on the screen
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    updateTimer();
+
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      gameOver();
+    }
+  }, 1000);
+}
+
+function updateTimer() {
+  timerElement.textContent = timeLeft;
+}
+
 function showQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
   questionElement.textContent = currentQuestion.question;
@@ -70,68 +132,58 @@ function showQuestion() {
 
   for (var i = 0; i < currentQuestion.choices.length; i++) {
     var choice = currentQuestion.choices[i];
-    var choiceElement = document.createElement("button");
-    choiceElement.textContent = (i + 1) + ". " + choice;
-    choiceElement.setAttribute("class", "choice");
-    choiceElement.setAttribute("value", choice);
-
-    choiceElement.addEventListener("click", handleChoiceClick);
-
-    choicesElement.appendChild(choiceElement);
+    var choiceButton = document.createElement("button");
+    choiceButton.classList.add("choice");
+    choiceButton.textContent = i + 1 + ". " + choice;
+    choiceButton.addEventListener("click", checkAnswer);
+    choicesElement.appendChild(choiceButton);
   }
 }
 
-// Handle the click event on a choice
-function handleChoiceClick(event) {
-  var selectedChoice = event.target.value;
-  var currentQuestion = questions[currentQuestionIndex];
+function checkAnswer(event) {
+  var selectedChoice = event.target;
+  var selectedAnswer = selectedChoice.textContent.slice(3);
 
-  if (selectedChoice === currentQuestion.correctAnswer) {
-    feedbackElement.textContent = "Correct!";
+  if (selectedAnswer === questions[currentQuestionIndex].answer) {
+    showMessage("Correct!");
   } else {
-    feedbackElement.textContent = "Wrong!";
+    showMessage("Wrong!");
+    timeLeft -= 10;
+    if (timeLeft < 0) {
+      timeLeft = 0;
+    }
+    updateTimer();
   }
 
   currentQuestionIndex++;
-
   if (currentQuestionIndex < questions.length) {
     showQuestion();
   } else {
-    endQuiz();
+    gameOver();
   }
 }
 
-// End the quiz
-function endQuiz() {
-  questionContainer.style.display = "none";
+function gameOver() {
+  clearInterval(timerInterval);
+  hideElement(questionContainer);
+  showElement(gameOverContainer);
+  finalScoreElement.textContent = timeLeft;
 }
 
-// Event listener for submitting initials and high score
-document.getElementById("score-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  var initials = initialsInput.value.trim();
-
-  if (initials !== "") {
-    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-    var newScore = { initials: initials, score: score };
-    highScores.push(newScore);
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-
-    displayHighScores();
-  }
-});
-
-// Function to display high scores
-function displayHighScores() {
-  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-  highScoresList.innerHTML = "";
-
-  for (var i = 0; i < highScores.length; i++) {
-    var scoreEntry = document.createElement("li");
-    scoreEntry.textContent = highScores[i].initials + " - " + highScores[i].score;
-    highScoresList.appendChild(scoreEntry);
-  }
+function showMessage(message) {
+  messageElement.textContent = message;
+  showElement(messageElement);
+  setTimeout(function () {
+    hideElement(messageElement);
+  }, 1000);
 }
 
-// Attach event listener to the Start Quiz button
-document.getElementById("start-btn").addEventListener("click", startQuiz);
+function hideElement(element) {
+  element.classList.add("hide");
+}
+
+function showElement(element) {
+  element.classList.remove("hide");
+}
+
+startButton.addEventListener("click", startQuiz);
